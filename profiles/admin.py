f7f0de6995d5896c0445e7d287a4cddea59a04af
nikochiko <kaustubh299792458@gmail.com>
2020-07-05 19:02:24 +0530

    Test

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Activities, Likes, Trip, Bio
+
+
+admin.site.register(Activities)
+admin.site.register(Likes)
+admin.site.register(Trip)
+admin.site.register(Bio)
