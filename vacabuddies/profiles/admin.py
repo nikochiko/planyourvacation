e5e0765233cf99bc010a6eb8672a0d096f1d224f
Restyled.io <commits@restyled.io>
2020-07-05 11:15:31 +0000

    Restyled by reorder-python-imports

--- vacabuddies/profiles/admin.py
+++ vacabuddies/profiles/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
+from .models import Activities
+from .models import Bio
+from .models import Likes
+from .models import Trip
 # Register your models here.
-from .models import Activities, Bio, Likes, Trip
